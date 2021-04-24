596ff6bdeac332c3241e4047083cafa3e190b26c
kumakuma0319 <whiteprankton@icloud.com>
2021-04-24 12:04:44 +0900

    複数投稿時の修正

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,12 +1,12 @@
 class Hotel < ApplicationRecord
   belongs_to :user
-  has_many_attached :images
+  has_one_attached :image
 
   with_options presence: true do
     validates :name
     validates :price
     validates :facilities
-    validates :images
+    validates :image
     validates :check_in_hour
     validates :check_in_minutes
     validates :check_out_hour
