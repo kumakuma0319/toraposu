631a972de1962fe8330d2aa5429dfc7f14e98954
kumakuma0319 <whiteprankton@icloud.com>
2021-04-24 12:33:50 +0900

    ユーザーのモデル単体テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname { Faker::Name.name }
+    email { Faker::Internet.free_email }
+    password { 'test123' }
+    password_confirmation { password }
   end
 end
